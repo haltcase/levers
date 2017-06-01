d639c8b937d30082b0d509759a93ae4253cf3ee4
Bo Lingen <lingenbw@gmail.com>
2017-06-01 05:35:25 +0000

    test: add tests for all current features

---
+++ tests/api.js
@@ -0,0 +1,191 @@
+import test from 'ava'
+import levers from '..'
+import { isAbsolute } from 'path'
+import { unlinkSync } from 'fs'
+
+const baseName = '__levers_temp_file_api__'
+
+test('set() :: stores value at the specified path', t => {
+  let storage = levers(baseName + 'set')
+
+  storage.set('keyOne', 'valueOne')
+  t.is(storage.get('keyOne'), 'valueOne')
+
+  unlinkSync(storage.path)
+})
+
+test('set() :: allows for setting deeply nested keys', t => {
+  let storage = levers(baseName + 'set-deep')
+
+  storage.set('deeply.nested.key', true)
+  t.deepEqual(storage.data, { deeply: { nested: { key: true } } })
+
+  unlinkSync(storage.path)
+})
+
+test('set() :: allows using an object to set multiple keys', t => {
+  let storage = levers(baseName + 'set-object')
+
+  storage.set({ one: 1, two: 2 })
+  t.deepEqual(storage.data, { one: 1, two: 2 })
+
+  unlinkSync(storage.path)
+})
+
+test('get() :: retrieves value at the specified path', t => {
+  let storage = levers(baseName + 'get')
+
+  storage.set('someKey', 42)
+  t.is(storage.get('someKey'), 42)
+
+  unlinkSync(storage.path)
+})
+
+test('get() :: returns provided value if the path is nil', t => {
+  let storage = levers(baseName + 'get-or')
+
+  storage.set('actualKey')
+  t.is(storage.get('nothingHere', 'default value'), 'default value')
+
+  unlinkSync(storage.path)
+})
+
+test('get() :: allows for accessing deeply nested keys', t => {
+  let storage = levers(baseName + 'get-deep')
+
+  storage.data = { deeply: { nested: { object: { key: 'value' } } } }
+  t.is(storage.get('deeply.nested.object.key'), 'value')
+
+  unlinkSync(storage.path)
+})
+
+test('has() :: returns true or false based on the path\'s existence', t => {
+  let storage = levers(baseName + 'has')
+
+  t.false(storage.has('someKey'))
+  storage.set('someKey', 'a value')
+  t.true(storage.has('someKey'))
+
+  unlinkSync(storage.path)
+})
+
+test('has() :: allows checking for deeply nested keys', t => {
+  let storage = levers(baseName + 'has-deep')
+
+  let path = 'deeply.nested.inner.property'
+  t.false(storage.has(path))
+  
+  storage.set(path, '42')
+  t.true(storage.has(path))
+
+  unlinkSync(storage.path)
+})
+
+test('del() :: remove the specified path from the storage', t => {
+  let storage = levers(baseName + 'del')
+
+  storage.set('property', 999999)
+  t.true(storage.has('property'))
+  
+  storage.del('property')
+  t.false(storage.has('property'))
+
+  unlinkSync(storage.path)
+})
+
+test('del() :: allows for removing deeply nested keys', t => {
+  let storage = levers(baseName + 'del-deep')
+
+  let path = 'deeply.nested.internal.property'
+  storage.set(path, true)
+  t.true(storage.has(path))
+
+  storage.del(path)
+  t.false(storage.has(path))
+
+  unlinkSync(storage.path)
+})
+
+test('clear() :: remove all keys from the storage', t => {
+  let storage = levers(baseName + 'clear')
+
+  for (let v of [1, 2, 3, 4, 5]) {
+    storage.set(`key${v}`, v)
+  }
+  
+  t.is(storage.size, 5)
+  
+  storage.clear()
+  t.is(storage.size, 0)
+
+  unlinkSync(storage.path)
+})
+
+test('size :: returns the number of keys in storage', t => {
+  let storage = levers(baseName + 'size')
+
+  t.is(storage.size, 0)
+
+  storage.set('something', 42)
+  t.is(storage.size, 1)
+
+  for (let v of [1, 2, 3, 4, 5]) {
+    storage.set(`key${v}`, v)
+  }
+  
+  t.is(storage.size, 6)
+
+  unlinkSync(storage.path)
+})
+
+test('data :: returns the entire storage set', t => {
+  let storage = levers(baseName + 'data-get')
+
+  t.deepEqual(storage.data, {})
+
+  storage.set('key', 'value')
+  t.deepEqual(storage.data, { key: 'value'  })
+
+  unlinkSync(storage.path)
+})
+
+test('data :: replaces the entire storage set', t => {
+  let storage = levers(baseName + 'data-set')
+
+  let expected = {
+    theme: 'dark',
+    config: {
+      showToolbar: false,
+      showSidebar: true,
+      autoLogin: false
+    }
+  }
+  
+  storage.data = expected
+  t.deepEqual(storage.data, expected)
+
+  unlinkSync(storage.path)
+})
+
+test('path :: returns the actual absolute file path', t => {
+  let storage = levers(baseName + 'path')
+
+  t.true(isAbsolute(storage.path))
+})
+
+test('iterator :: allows direct iteration over the storage set', t => {
+  let storage = levers(baseName + 'iterator')
+
+  t.true(Symbol.iterator in storage)
+  storage.data = { one: 'one', two: 'two', three: 'three' }
+  
+  let keys = []
+  for (const [k, v] of storage) {
+    t.true(k === v)
+    keys.push(k)
+  }
+  
+  t.deepEqual(keys, ['one', 'two', 'three'])
+
+  unlinkSync(storage.path)
+})
---
+++ tests/file-handling.js
@@ -0,0 +1,54 @@
+import test from 'ava'
+import levers from '..'
+import { isAbsolute } from 'path'
+import { existsSync, unlinkSync } from 'fs'
+
+const baseName = '__levers_temp_test_file__'
+
+test('exposes path resolution logic as a static `resolve()` method', t => {
+  t.is(typeof levers.resolve, 'function')
+  t.is(typeof levers.resolve(baseName), 'string')
+  t.true(isAbsolute(levers.resolve(baseName)))
+})
+
+test('exposes file existence helper as a static `exists()` method', t => {
+  t.is(typeof levers.exists, 'function')
+  t.false(levers.exists('some-non-existent-file-name'))
+  
+  let name = '__levers_temp_settings_file__'
+  let settings = levers(name)
+  settings.set('someKey', 'someValue')
+
+  t.true(levers.exists(name))
+
+  unlinkSync(levers.resolve(name))
+})
+
+test('creates the target file lazily if it does not exist', t => {
+  let name = baseName + 'new'
+  let path = levers.resolve(name)
+  t.false(existsSync(path))
+
+  let settings = levers(name)
+  t.false(existsSync(path))
+
+  settings.set('someKey', 'someValue')
+  t.true(existsSync(path))
+
+  unlinkSync(path)
+})
+
+test('access the target file if it already exists', t => {
+  let name = baseName + 'existing'
+  let path = levers.resolve(name)
+
+  let initializer = levers(name)
+  initializer.set('someKey', 'someValue')
+  t.true(existsSync(path))
+  initializer = null
+
+  let settings = levers(name)
+  t.is(settings.get('someKey'), 'someValue')
+
+  unlinkSync(path)
+})
